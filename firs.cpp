0e20caf4e19e7bd04e6c65cb905824250662c585
baharmoradi <bhrmoradi.1383.1383@gmail.com>
2024-03-03 17:53:58 +0330

    this is the first commit

---
+++ firs.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int sum(int a, int b){
+    return a+b;
+}
+int main(){
+    char c=66, v="B";
+    int m = 54, n = 66;
+    float f1 = 31,2 , f2= 54,3;
+    cout <<"sum of m, v: "<<sum (m,v) << <<sum (f1,f2): 
+return 0;
+}
